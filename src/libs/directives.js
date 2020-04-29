da2103e1d5f7d6de6d021d5404266ef67472c834
李晶 <lijing@bingosoft.net>
2020-04-29 15:07:12 +0800

    优化v-security指令支持warn修饰符

--- src/libs/directives.js
+++ src/libs/directives.js
@@ -1,25 +1,29 @@
 import permUtils from './security/permission';
 
-function displayNotSecurityContent(el,value){
+function displayNotSecurityContent(el,value,binding){
   let hasPerms=permUtils.hasPerms(value);
   if(!hasPerms){
-    let notSecurityHtml=`<div class="ivu-alert ivu-alert-warning ivu-alert-with-icon ivu-alert-with-desc"><span class="ivu-alert-icon"><i class="ivu-icon ivu-icon-ios-alert-outline"></i></span> <span class="ivu-alert-message">
-        页面错误
-        </span> <span class="ivu-alert-desc">
-          未授权的操作：${value}
-        </span> <!----></div>`
-    el.innerHTML=notSecurityHtml;
+    if(binding.modifiers.warn){
+      let notSecurityHtml=`<div class="ivu-alert ivu-alert-warning ivu-alert-with-icon ivu-alert-with-desc"><span class="ivu-alert-icon"><i class="ivu-icon ivu-icon-ios-alert-outline"></i></span> <span class="ivu-alert-message">
+          页面错误
+          </span> <span class="ivu-alert-desc">
+            未授权的操作：${value}
+          </span> <!----></div>`
+      el.innerHTML=notSecurityHtml;
+    }else{
+      el.parentNode && el.parentNode.removeChild(el);
+    }
   }
 }
 export default function CustomDirectives(Vue) {
   Vue.directive('security', {
-        inserted: function (el,binding,vnode,oldVnode) {
-            var value=binding.value||{};
-            displayNotSecurityContent(el,value);
-        },
-        componentUpdated:function (el,binding,vnode,oldVnode) {
-            var value=binding.value||{};
-            displayNotSecurityContent(el,value);
-        }
-    });
+      inserted: function (el,binding,vnode,oldVnode) {
+          var value=binding.value||{};
+          displayNotSecurityContent(el,value,binding);
+      },
+      componentUpdated:function (el,binding,vnode,oldVnode) {
+          var value=binding.value||{};
+          displayNotSecurityContent(el,value,binding);
+      }
+  });
 }
